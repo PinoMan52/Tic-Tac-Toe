7ad9d653f4d56d43a0f4e0d6397a7ea3874fe673
PinoMan52 <139012820+PinoMan52@users.noreply.github.com>
2023-07-10 11:53:57 -0400

    Create ttt_functions.hpp

---
+++ ttt_functions.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+void printBoard(std::vector<char> board);
+bool winCheck(std::vector<char> board, std::string winner);
+void winMessage(std::vector<char> board, std::string winner);
